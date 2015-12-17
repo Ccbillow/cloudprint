b7c33f845cb641c5776342575a01fd062aa6e66b
tao.cheng <ctaoaoo@iCloud.com>
2015-12-17 20:29:45 +0800

    fix

--- src/main/java/cn/cqupt/service/impl/PrintFileServiceImpl.java
+++ src/main/java/cn/cqupt/service/impl/PrintFileServiceImpl.java
@@ -20,7 +20,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
